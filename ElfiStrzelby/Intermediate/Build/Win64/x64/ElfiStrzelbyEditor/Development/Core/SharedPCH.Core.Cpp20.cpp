789625c916389a11b6a52c8017714c523ea58d35
Amarbach <amarbach@interia.pl>
2023-10-05 22:24:00 +0200

    Project Start

---
+++ ElfiStrzelby/Intermediate/Build/Win64/x64/ElfiStrzelbyEditor/Development/Core/SharedPCH.Core.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.37.32824
+#include "D:/Projekty/Unreal/ElfiStrzelby/ElfiStrzelby/Intermediate/Build/Win64/x64/ElfiStrzelbyEditor/Development/Core/SharedPCH.Core.Cpp20.h"
---
+++ ElfiStrzelby/Intermediate/Build/Win64/x64/ElfiStrzelbyEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "D:/Projekty/Unreal/ElfiStrzelby/ElfiStrzelby/Intermediate/Build/Win64/x64/ElfiStrzelbyEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
